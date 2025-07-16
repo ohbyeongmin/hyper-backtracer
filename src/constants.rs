ce79c71abea4bfa04653ccd338eedb6290529bb2
0x2biimi <0x2biimi@gmail.com>
2025-07-16 22:56:25 +0900

    chore: mod default candle intervals

--- src/constants.rs
+++ src/constants.rs
@@ -1,2 +1,2 @@
 pub const API_URL: &str = "http://api.hyperliquid.xyz";
-pub const DEFAULT_CANDLE_INTERVALS: &str = "1m,5m,15m,30m,1h,4h,1d,1w,1M";
+pub const DEFAULT_CANDLE_INTERVALS: &str = "1m,3m,5m,15m,30m,1h,2h,4h,8h,12h,1d,3d,1w,1M";
