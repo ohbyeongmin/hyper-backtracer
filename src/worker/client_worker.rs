5ca07e03cb3022151c58c9f5fca66daafd69b67c
0x2biimi <0x2biimi@gmail.com>
2025-07-19 22:18:19 +0900

    refactor: mod client worker

--- src/worker/client_worker.rs
+++ src/worker/client_worker.rs
@@ -1,79 +1,31 @@
-use anyhow::{Context, Result};
+use crate::{
+    common::dto::client::{CandleSnapshotBody, ClientResponse},
+    worker::worker_manager::ClientInterface,
+};
+use anyhow::Context;
 use tokio::sync::{mpsc, oneshot};
 
-use crate::{
-    client,
-    commander::{self, commands::ClientCommand},
-    helpers,
-    models::{CandleSnapshotBody, CandleSnapshotPayload, CandleSnapshotResponse, TimeRawData},
-};
-
-const BODY_TYPE: &str = "candleSnapshot";
-
-#[derive(Debug, Clone)]
-pub enum Action {
-    GetAllCandle {
-        coin_symbol: String,
-        interval: String,
-    },
-    GetPartCandle {
-        coin_symbol: String,
-        interval: String,
-        start_time: TimeRawData,
-    },
-}
-
 pub struct ClientWorker {
-    tx_commander: mpsc::Sender<ClientCommand>,
+    tx: mpsc::Sender<ClientInterface>,
 }
 
 impl ClientWorker {
-    pub fn new(tx_commander: mpsc::Sender<ClientCommand>) -> Self {
-        Self { tx_commander }
+    pub fn new(tx: mpsc::Sender<ClientInterface>) -> Self {
+        Self { tx }
     }
 
-    pub async fn start(&self, action: Action) -> Result<Vec<CandleSnapshotResponse>> {
-        let (resp_tx, resp_rx) = oneshot::channel::<client::Response>();
-        let req = Self::build_request(action);
-        let cmd = commander::ClientCommand::GetCandle { req, resp: resp_tx };
+    pub async fn action(&self, payload: CandleSnapshotBody) -> ClientResponse {
+        let (resp_tx, resp_rx) = oneshot::channel::<ClientResponse>();
+        let cmd = ClientInterface::GetCandleSnapshot {
+            req: payload,
+            resp: resp_tx,
+        };
 
-        self.tx_commander
+        self.tx
             .send(cmd)
             .await
             .context("failed client worker: send")?;
 
         resp_rx.await.context("failed client worker: response")?
-    }
-
-    fn build_request(action: Action) -> CandleSnapshotBody {
-        let end_time = helpers::AppDateTime::now().to_milliseconds();
-
-        let payload = match action {
-            Action::GetAllCandle {
-                coin_symbol,
-                interval,
-            } => CandleSnapshotPayload {
-                coin: coin_symbol,
-                interval,
-                start_time: 0,
-                end_time,
-            },
-
-            Action::GetPartCandle {
-                coin_symbol,
-                interval,
-                start_time,
-            } => CandleSnapshotPayload {
-                coin: coin_symbol,
-                interval,
-                start_time,
-                end_time,
-            },
-        };
-
-        CandleSnapshotBody {
-            body_type: BODY_TYPE.to_string(),
-            payload,
-        }
     }
 }
